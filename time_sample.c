14ce5d4e33999bc6da62faf388274d7319954388
Kevin <suokunstar@gmail.com>
2020-10-13 10:01:27 -0400

    Create time_sample.c

---
+++ time_sample.c
@@ -0,0 +1,13 @@
+#include "mpi.h"
+#include <stdio.h>
+
+int main( int argc, char *argv[] )
+{
+	double t1, t2 = 0;
+
+	MPI_Init( 0, 0 );
+	sleep(10);
+	printf("MPI_Wtime measured a 1 second sleep to be: %1.2f\n", t2-t1);fflush(stdout);
+	MPI_Finalize( );
+	return 0;
+}
